95a2f90e3077877d4c1ccbc48845b86d995f5b1f
srepac <68381903+srepac@users.noreply.github.com>
2022-03-05 10:14:54 -0800

    Update otgnet.sh

--- otgnet.sh
+++ otgnet.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+### 
+# Filename:  otgnet.sh
+# 
+# This script takes care of adding the usb0 ethernet passthru DHCP range to /etc/dnsmasq.conf
+# ... so that the main dnsmasq service will listen for DHCP requests for ap0 and usb0 on pikvm
+#
+# This is downloaded and run by the hotspot[.rpi] scripts
+###
 DHCPRANGE=$( systemctl status kvmd-otgnet | grep range | awk -F'range=' '{print $2}' | cut -d' ' -f1 | uniq )
 if [[ $DHCPRANGE == "" ]]; then
         echo "No otgnet: overrides found.  Exiting."
@@ -12,7 +20,7 @@ if [[ $( grep -c $DHCPUSB0 /etc/dnsmasq.conf ) -ne 1 ]]; then
     # disable kvmd-otgnet-dnsmasq before adding entry and restarting dnsmasq
     systemctl disable kvmd-otgnet-dnsmasq
 
-    echo "missing entry in /etc/dnsmasq.conf.  Adding and restarting dnsmasq ..."
+    echo "-> Missing entry in /etc/dnsmasq.conf.  Adding and restarting dnsmasq ..."
     if [ $( grep -i ^name= /etc/os-release | cut -d'"' -f 2 | cut -d' ' -f 1 ) == "Arch" ]; then rw; fi
     echo "# Add DHCP entry for usb ethernet passthru adapter" >> /etc/dnsmasq.conf
     echo $DHCPUSB0 >> /etc/dnsmasq.conf
